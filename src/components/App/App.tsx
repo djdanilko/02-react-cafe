32f75b7eb9232f07b732899c4cbd137037ae385d
Daniel <djdanilko02@gmail.com>
2026-06-19 17:56:08 +0200

    fin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,1 +1,59 @@
+import { useState } from 'react';
 
+import type { VoteType, Votes } from '../../types/votes';
+import CafeInfo from '../CafeInfo/CafeInfo.tsx';
+import VoteOptions from '../VoteOptions/VoteOptions.tsx';
+import VoteStats from '../VoteStats/VoteStats.tsx';
+import Notification from '../Notification/Notification.tsx';
+import css from './App.module.css';
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveVotes = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveVotes={positiveVotes}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.tsx";
+
+// Нормалізація стилів
+import "modern-normalize/modern-normalize.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
